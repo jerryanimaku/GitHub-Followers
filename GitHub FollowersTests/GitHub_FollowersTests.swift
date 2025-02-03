f267a3d9e989a78c313f7ab353cda0efa814d699
Jeremiah Animaku <jeremiahanimaku@gmail.com>
2025-02-03 03:10:36 -0600

    Initial Commit

---
+++ GitHub FollowersTests/GitHub_FollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_FollowersTests.swift
+//  GitHub FollowersTests
+//
+//  Created by Jeremiah on 2/3/25.
+//
+
+import Testing
+@testable import GitHub_Followers
+
+struct GitHub_FollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
